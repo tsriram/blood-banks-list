f6c8a580565a28eeb0bb9dc7ba613a1f3712c8da
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-08-16 19:58:53 +0530

    fixing app render issue

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ import BloodBanksContainer from './components/BloodBanksContainer';
 
 import './assets/sass/main.scss';
 
-document.addEventListener('DOMContentLoaded', function () {
+function renderApp(){
   let appElement = document.getElementById('bloodbanks');
   let classNames = appElement.className;
   ReactDOM.render(<BloodBanksContainer />, appElement);
-});
+}
+
+if (document.readyState === "complete" || document.readyState === "interactive") {
+  renderApp();
+}else {
+  document.addEventListener('DOMContentLoaded', renderApp);
+}
